f4e8fa4e6b49947c3976be53aa53acb9ae939582
issam <issammez44@gmail.com>
2023-01-10 23:34:23 +0100

    fetch the omdb api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React from "react";
+import { useEffect } from "react";
+import './App.css';
+import SearchIcon from './search.svg'
+
+const API_URL = 'http://www.omdbapi.com?apikey=a5ac6dbd'
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        console.log(data.Search);
+    }
+
+    useEffect(() =>{
+        searchMovies('fight club');
+    }, []);
+
     return(
         <>
             <h1>Hello, World!</h1>
